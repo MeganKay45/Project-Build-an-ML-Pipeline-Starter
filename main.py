850f58046dd74ea85f8aef75719078779109958e
MeganKay45 <mkwhite45@yahoo.com>
2026-06-02 23:26:31 +0000

    MFlow update

--- main.py
+++ main.py
@@ -50,9 +50,10 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-             _ = mlflow.run(
-                f"{config['main']['components_repository']}/basic_cleaning",
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
                 "main",
+                env_manager="conda",
                 parameters={
                     "input_artifact": "sample.csv:latest",
                     "output_artifact": "clean_sample.csv",
@@ -65,7 +66,7 @@ def go(config: DictConfig):
 
         if "data_check" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/data_check",
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
                 "main",
                 env_manager="conda",
                 parameters={
@@ -76,6 +77,7 @@ def go(config: DictConfig):
                     "max_price": config["etl"]["max_price"],
                 },
             )
+
 
         if "data_split" in active_steps:
             _ = mlflow.run(
@@ -95,7 +97,8 @@ def go(config: DictConfig):
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)
 
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/train_random_forest",
+                os.path.join(hydra.utils.get_original_cwd(),
+                            "src", "train_random_forest"),
                 "main",
                 env_manager="conda",
                 parameters={
@@ -108,6 +111,8 @@ def go(config: DictConfig):
                     "max_tfidf_features": config["modeling"]["max_tfidf_features"],
                 },
             )
+
+
 
 
         if "test_regression_model" in active_steps:
